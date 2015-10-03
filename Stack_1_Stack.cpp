679fd77d9c8bb891bea275bcbe791fd6b0b78a57
AndrewGREMORI <andrewgremori@gmail.com>
2015-10-03 10:15:22 +0300

    Create Stack_1_Stack.cpp

---
+++ Stack_1_Stack.cpp
@@ -0,0 +1,28 @@
+
+#include "stdafx.h"
+#include "stack.h"
+
+Node *first(int data)
+{
+	Node *top = new Node;
+	top->data = data;
+	top->field = 0;
+	return top;
+}
+
+void push(Node **top, int data)
+{
+	Node *new_field = new Node;
+	new_field->data = data;
+	new_field->field = *top;
+	*top = new_field;
+}
+
+int pop(Node **top)
+{
+	int temp = (*top)->data;
+	Node *new_field = *top;
+	*top = (*top)->field;
+	delete new_field;
+	return temp;
+}
