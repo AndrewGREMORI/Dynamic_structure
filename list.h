cf150881015e0a03152579e26117af48852c094c
AndrewGREMORI <andrewgremori@gmail.com>
2015-10-05 16:30:27 +0300

    Create list.h

---
+++ list.h
@@ -0,0 +1,17 @@
+
+Struct Node
+{
+  int data;
+  Node *next;
+}
+
+  Node *first(int data);
+  
+  void add(node *field_beg, int data);
+  
+  Node *find(node *const field_beg, int data);
+  
+  Node *insert(noe *const field_beg, int key, int data);
+  
+  Node *insert_befor(noe **field_beg, int key, int data);
+  
